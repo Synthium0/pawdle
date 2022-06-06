2ca00a5e19fa98e63fc6d3193e21222c2b58e701
Synthium <leapingcloud10@gmail.com>
2022-06-06 12:30:43 -0400

    Initial Commit

---
+++ src/Game.tsx
@@ -0,0 +1,280 @@
+import { useEffect, useRef, useState } from "react";
+import { Row, RowState } from "./Row";
+import dictionary from "./dictionary.json";
+import { Clue, clue, describeClue, violation } from "./clue";
+import { Keyboard } from "./Keyboard";
+import targetList from "./targets.json";
+import {
+  Difficulty,
+  pick,
+  resetRng,
+  speak,
+  urlParam,
+} from "./util";
+
+enum GameState {
+  Playing,
+  Won,
+  Lost,
+}
+
+interface GameProps {
+  maxGuesses: number;
+  hidden: boolean;
+  difficulty: Difficulty;
+  colorBlind: boolean;
+  keyboardLayout: string;
+}
+
+const targets = targetList.slice(0, targetList.indexOf("murky") + 1); // Words no rarer than this one
+const minLength = 4;
+const defaultLength = 5;
+const maxLength = 11;
+const limitLength = (n: number) =>
+  n >= minLength && n <= maxLength ? n : defaultLength;
+
+function randomTarget(wordLength: number): string {
+  const eligible = targets.filter((word) => word.length === wordLength);
+  let candidate: string;
+  do {
+    candidate = pick(eligible);
+  } while (/\*/.test(candidate));
+  return candidate;
+}
+
+
+
+let initChallenge = "";
+let challengeError = false; // Just to make sure that the code doesnt get angry about challenge errors
+// reason why it will get mad is because I removed the challenge link.
+// this is because you can use the same link over and over, and you will get paw token for it.
+
+
+
+function parseUrlLength(): number {
+  const lengthParam = urlParam("length");
+  if (!lengthParam) return defaultLength;
+  return limitLength(Number(lengthParam));
+}
+
+function parseUrlGameNumber(): number {
+  const gameParam = urlParam("game");
+  if (!gameParam) return 1;
+  const gameNumber = Number(gameParam);
+  return gameNumber >= 1 && gameNumber <= 1000 ? gameNumber : 1;
+}
+
+function Game(props: GameProps) {
+  const [gameState, setGameState] = useState(GameState.Playing);
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const [currentGuess, setCurrentGuess] = useState<string>("");
+  const [challenge, setChallenge] = useState<string>(initChallenge);
+  const [wordLength, setWordLength] = useState(
+    challenge ? challenge.length : parseUrlLength()
+  );
+  const [gameNumber, setGameNumber] = useState(parseUrlGameNumber());
+  const [target, setTarget] = useState(() => {
+    resetRng();
+    // Skip RNG ahead to the parsed initial game number:
+    for (let i = 1; i < gameNumber; i++) randomTarget(wordLength);
+    return challenge || randomTarget(wordLength);
+  });
+  const [hint, setHint] = useState<string>(
+    challengeError
+      ? `Invalid challenge string, playing random game.`
+      : `Make your first guess!`
+  );
+  
+  useEffect(() => {
+  
+  }, [wordLength, gameNumber]);
+  const tableRef = useRef<HTMLTableElement>(null);
+  const startNextGame = () => {
+    if (challenge) {
+      // Clear the URL parameters:
+      window.history.replaceState({}, document.title, window.location.pathname);
+    }
+    setChallenge("");
+    const newWordLength = limitLength(wordLength);
+    setWordLength(newWordLength);
+    setTarget(randomTarget(newWordLength));
+    setHint("");
+    setGuesses([]);
+    setCurrentGuess("");
+    setGameState(GameState.Playing);
+    setGameNumber((x) => x + 1);
+  };
+
+  
+
+  const onKey = (key: string) => {
+    if (gameState !== GameState.Playing) {
+      if (key === "Enter") {
+        startNextGame();
+      }
+      return;
+    }
+    if (guesses.length === props.maxGuesses) return;
+    if (/^[a-z]$/i.test(key)) {
+      setCurrentGuess((guess) =>
+        (guess + key.toLowerCase()).slice(0, wordLength)
+      );
+      tableRef.current?.focus();
+      setHint("");
+    } else if (key === "Backspace") {
+      setCurrentGuess((guess) => guess.slice(0, -1));
+      setHint("");
+    } else if (key === "Enter") {
+      if (currentGuess.length !== wordLength) {
+        setHint("Too short");
+        return;
+      }
+      if (!dictionary.includes(currentGuess)) {
+        setHint("Not a valid word");
+        return;
+      }
+      for (const g of guesses) {
+        const c = clue(g, target);
+        const feedback = violation(props.difficulty, c, currentGuess);
+        if (feedback) {
+          setHint(feedback);
+          return;
+        }
+      }
+      setGuesses((guesses) => guesses.concat([currentGuess]));
+      setCurrentGuess((guess) => "");
+
+      const gameOver = (verbed: string) =>
+        `You ${verbed}! The answer was ${target.toUpperCase()}. (Enter to ${
+          challenge ? "play a random game" : "play again"
+        })`;
+
+      if (currentGuess === target) {
+        setHint(gameOver("won"));
+        setGameState(GameState.Won);
+        console.log("YOU WIN 10 PAW!!! WOOOOOO")
+      } else if (guesses.length + 1 === props.maxGuesses) {
+        setHint(gameOver("lost"));
+        setGameState(GameState.Lost);
+      } else {
+        setHint("");
+        speak(describeClue(clue(currentGuess, target)));
+      }
+    }
+  };
+
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (!e.ctrlKey && !e.metaKey) {
+        onKey(e.key);
+      }
+      if (e.key === "Backspace") {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [currentGuess, gameState]);
+
+  let letterInfo = new Map<string, Clue>();
+  const tableRows = Array(props.maxGuesses)
+    .fill(undefined)
+    .map((_, i) => {
+      const guess = [...guesses, currentGuess][i] ?? "";
+      const cluedLetters = clue(guess, target);
+      const lockedIn = i < guesses.length;
+      if (lockedIn) {
+        for (const { clue, letter } of cluedLetters) {
+          if (clue === undefined) break;
+          const old = letterInfo.get(letter);
+          if (old === undefined || clue > old) {
+            letterInfo.set(letter, clue);
+          }
+        }
+      }
+      return (
+        <Row
+          key={i}
+          wordLength={wordLength}
+          rowState={
+            lockedIn
+              ? RowState.LockedIn
+              : i === guesses.length
+              ? RowState.Editing
+              : RowState.Pending
+          }
+          cluedLetters={cluedLetters}
+        />
+      );
+    });
+
+  return (
+    <div className="Game" style={{ display: props.hidden ? "none" : "block" }}>
+      <div className="Game-options">
+        <label htmlFor="wordLength">Letters:</label>
+        <input
+          type="range"
+          min={minLength}
+          max={maxLength}
+          id="wordLength"
+          disabled={
+            gameState === GameState.Playing &&
+            (guesses.length > 0 || currentGuess !== "" || challenge !== "")
+          }
+          value={wordLength}
+          onChange={(e) => {
+            const length = Number(e.target.value);
+            resetRng();
+            setGameNumber(1);
+            setGameState(GameState.Playing);
+            setGuesses([]);
+            setCurrentGuess("");
+            setTarget(randomTarget(length));
+            setWordLength(length);
+            setHint(`${length} letters`);
+          }}
+        ></input>
+        <button
+          style={{ flex: "0 0 auto" }}
+          disabled={gameState !== GameState.Playing || guesses.length === 0}
+          onClick={() => {
+            setHint(
+              `The answer was ${target.toUpperCase()}. (Enter to play again)`
+            );
+            setGameState(GameState.Lost);
+            (document.activeElement as HTMLElement)?.blur();
+          }}
+        >
+          Give up
+        </button>
+      </div>
+      <table
+        className="Game-rows"
+        tabIndex={0}
+        aria-label="Table of guesses"
+        ref={tableRef}
+      >
+        <tbody>{tableRows}</tbody>
+      </table>
+      <p
+        role="alert"
+        style={{
+          userSelect: /https?:/.test(hint) ? "text" : "none",
+          whiteSpace: "pre-wrap",
+        }}
+      >
+        {hint || `\u00a0`}
+      </p>
+      <Keyboard
+        layout={props.keyboardLayout}
+        letterInfo={letterInfo}
+        onKey={onKey}
+      />
+
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/PawInput.tsx
@@ -0,0 +1,30 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+// TODO: make a limiter per ip address
+// and make a websocket server to recieve the address
+// then, in App.tsx, make it send 100 PAW to that address when you get the answer correct
+
+
+}
+
+export function PawInput() {
+  return (
+      
+     <form>
+         <label>
+             PAW Address: { }
+             <input type="text" name="Address" />
+         </label>
+         <input type="button" value="Submit" />
+         <label id="AfterMessage"> </label>
+         <script>
+             button.addEventListener('click', updateButton);
+
+         </script>
+     </form>
+    
+
+  );
+}
